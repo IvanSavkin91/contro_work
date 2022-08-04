acd689502f2903046120ed8edbf9fcecbc37e53c
IvanSavkin <adoys1@gmail.com>
2022-08-04 21:29:20 +0300

    Вывод введенных данных

--- Program.cs
+++ Program.cs
@@ -6,3 +6,9 @@ for ( int i = 0; i < array.Length; i++)
     Console.WriteLine("Введите массив");
     array[i] = Console.ReadLine()!;
 }
+for ( int i = 0; i < array.Length; i++)
+
+{
+    Console.Write(array [i] + "\t");
+    
+}
