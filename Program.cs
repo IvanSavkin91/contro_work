1663634078c42dcf50d434e40792d3337009e46c
IvanSavkin <adoys1@gmail.com>
2022-08-04 21:33:42 +0300

    отсекаем длинну в 3 символа

--- Program.cs
+++ Program.cs
@@ -12,3 +12,14 @@ for ( int i = 0; i < array.Length; i++)
     Console.Write(array [i] + "\t");
     
 }
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Массив, содержащий 3 символа: ");
+for ( int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i] + "\t" );
+    }
+    
+}
